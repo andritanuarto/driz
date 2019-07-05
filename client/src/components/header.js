7b8c5b106c00b72b5f4cbb97c5546be3e7565aa9
Andri Tanuarto <andritanuarto@gmail.com>
2019-07-04 20:13:41 -0700

    Refactor header component and add dropdown state

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,17 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState, Fragment } from 'react';
 
-class Header extends Component {
+const Header = () => {
+  const [headerState, setdropdown] = useState({
+    dropdownOpen: false,
+  });
 
-  render() {
-    return (
-      <div className="header">
-        <ul>
-          <li>Login</li>
-          <li>Register</li>
-        </ul>
-      </div>
-    );
-  }
+  const { dropdownOpen } = headerState;
+
+  const dropdownToggle = (e) => setdropdown({
+    ...setdropdown,
+    dropdownOpen: !dropdownOpen
+  });
+
+
+  console.log(headerState);
+
+  return (
+    <div className="header">
+      <div className="driz"/>
+      <button onClick={dropdownToggle}>click</button>
+      <ul>
+        <li>Login</li>
+        <li>Register</li>
+      </ul>
+
+    </div>
+  );
 }
 
 export default Header;
