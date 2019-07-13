d5f394b9bf73ff9a23a8617dadd5920222ad4d66
Andri Tanuarto <andritanuarto@gmail.com>
2019-07-13 09:05:29 -0700

    Style header component

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,23 +1,13 @@
-import React, { useState, Fragment } from 'react';
+import React from 'react';
 import InlineSVG from 'svg-inline-react'
 import { SVG } from '../utils/svg'
 
 const Header = () => {
-  const [headerState, setdropdown] = useState({
-    dropdownOpen: false,
-  });
-
-  const { dropdownOpen } = headerState;
-
-  const dropdownToggle = (e) => setdropdown({
-    ...setdropdown,
-    dropdownOpen: !dropdownOpen
-  });
-
   return (
     <div className="header">
-      <InlineSVG  className="driz" src={SVG.DRIZ} raw width="50" height="15"/>
-
+      <i className="material-icons">menu</i>
+      <InlineSVG  className="driz" src={SVG.DRIZ} raw width="50" height="20"/>
+      <i className="material-icons">search</i>
     </div>
   );
 }
